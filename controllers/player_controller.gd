54191b80e1f19c1c9f06039868619309111115cb
Zakharii Furmanets <zfurmanec@gmail.com>
2024-09-21 15:13:15 +0200

    start moving samurai logic from PC to character itself

--- controllers/player_controller.gd
+++ controllers/player_controller.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-@onready var hsm: LimboHSM = %LimboHSM
-@onready var idle_state: LimboState = %Idle
-@onready var move_state: LimboState = %Run
-@onready var air_state: AirState = %Air
-@onready var attack_state: AttackState = %Attack  # Додаємо стан атаки
-@onready var death_state :DeathState = %Death
-
 @onready var character: Character = %GlitchSamurai
+
+@onready var hsm: StateMachine = %GlitchSamurai/%LimboHSM
+@onready var idle_state: LimboState = %GlitchSamurai/%Idle
+@onready var run_state: RunState =%GlitchSamurai/%Run
+@onready var air_state: AirState =%GlitchSamurai/%Air
+@onready var attack_state: AttackState =%GlitchSamurai/%Attack
+@onready var death_state: DeathState = %GlitchSamurai/%Death
 
 # Параметри для контролю руху
 @export var speed: float = 150.0
@@ -30,8 +30,8 @@ func _ready() -> void:
 	_init_state_machine()
 
 func _init_state_machine() -> void:
-	hsm.add_transition(idle_state, move_state, idle_state.EVENT_FINISHED)
-	hsm.add_transition(move_state, idle_state, move_state.EVENT_FINISHED)
+	hsm.add_transition(idle_state, run_state, idle_state.EVENT_FINISHED)
+	hsm.add_transition(run_state, idle_state, run_state.EVENT_FINISHED)
 	hsm.add_transition(air_state, idle_state, air_state.EVENT_FINISHED)
 	
 	hsm.add_transition(attack_state, idle_state, attack_state.EVENT_FINISHED)
@@ -60,10 +60,10 @@ func _process(delta: float) -> void:
 	# Перевірка на переміщення (входження в стан MoveState)
 	elif Input.get_axis("move_left", "move_right") != 0 and hsm.get_active_state() != air_state and hsm.get_active_state() != attack_state:
 		# Переходимо в стан руху, тільки якщо персонаж не в повітрі
-		if hsm.get_active_state() != move_state and character.is_on_floor():
+		if hsm.get_active_state() != run_state and character.is_on_floor():
 			if hsm.is_active():
-				hsm.change_active_state(move_state)
+				hsm.change_active_state(run_state)
 	
 	# Перевірка на повернення в IdleState, якщо персонаж не рухається
-	elif hsm.get_active_state() == move_state and Input.get_axis("move_left", "move_right") == 0 and character.is_on_floor():
+	elif hsm.get_active_state() == run_state and Input.get_axis("move_left", "move_right") == 0 and character.is_on_floor():
 		hsm.change_active_state(idle_state)
